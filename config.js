7486959febd2fd7da12f47dcce23230ccccbf2e2
Phucle-GPS <Phucle.rfc@gmail.com>
2026-09-23 21:18:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// URL Web App của Google Apps Script
+const API_URL = "https://script.google.com/macros/s/AKfycbyoNsIGi7o1l0SEruo0S2CDaP2vpukitraOBQUBJcnM-nBrhl163thMQa05pi8hYeSl/exec";
---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = "gnsc-v1";
+const ASSETS = ["./","./index.html","./login.html","./cabinets.js","./config.js","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
+self.addEventListener("activate", e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener("fetch", e=>{
+  const u = new URL(e.request.url);
+  if(e.request.method!=="GET" || u.origin!==location.origin) return; // API luôn đi mạng
+  e.respondWith(fetch(e.request).then(r=>{ const cp=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return r; }).catch(()=>caches.match(e.request)));
+});
