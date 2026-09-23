938aef5d4aadfc25123c739642bc3d0899a8718a
Phucle-GPS <Phucle.rfc@gmail.com>
2026-09-23 23:54:40 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 // URL Web App của Google Apps Script
 const API_URL = "https://script.google.com/macros/s/AKfycbyoNsIGi7o1l0SEruo0S2CDaP2vpukitraOBQUBJcnM-nBrhl163thMQa05pi8hYeSl/exec";
+
+// Mã bản đồ Google My Maps (phần sau "mid=" trong link chia sẻ). Để trống nếu chưa có.
+const MYMAPS_ID = "19ZmQyyQmxDF1V72rv0X8V43ps9KT3Ak";
